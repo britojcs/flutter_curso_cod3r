a91a26aa75200d4e06bfac0d06d47f71e58657ba
Brito <brito@Britos-MacBook-Pro.local>
2020-03-01 14:37:27 -0300

    projeto despesas pessoais - teclado numérico e modal no form

--- projetos/p2_expenses/lib/main.dart
+++ projetos/p2_expenses/lib/main.dart
@@ -1,8 +1,10 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:p2_expenses/components/transaction_form.dart';
-import 'package:p2_expenses/components/transaction_list.dart';
-import 'package:p2_expenses/components/transaction_user.dart';
-import 'package:p2_expenses/models/transaction.dart';
+
+import 'components/transaction_list.dart';
+import 'models/transaction.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -15,13 +17,63 @@ class ExpensesApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Sapato',
+      value: 310.75,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta Luz',
+      value: 98.30,
+      date: DateTime.now(),
+    ),
+  ];
+
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+
+    Navigator.of(context).pop();
+  }
+
+  _openTransactionFormModal(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (_) {
+        return TransactionForm(_addTransaction);
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Despesas Pessoais'),
         backgroundColor: Colors.cyan,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.playlist_add),
+            onPressed: () => _openTransactionFormModal(context),
+          )
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -33,9 +85,13 @@ class MyHomePage extends StatelessWidget {
                 elevation: 3,
               ),
             ),
-            TransactionUser(),
+            TransactionList(_transactions),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.playlist_add),
+        onPressed: () => _openTransactionFormModal(context),
       ),
     );
   }
