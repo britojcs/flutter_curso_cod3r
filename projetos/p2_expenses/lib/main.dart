7ed58c23024eea27b441e8c322cb2dcf57deee7d
Brito <brito@Britos-MacBook-Pro.local>
2020-03-15 09:14:19 -0300

    projeto despesas pessoais - modo paisagem

--- projetos/p2_expenses/lib/main.dart
+++ projetos/p2_expenses/lib/main.dart
@@ -49,6 +49,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _transactions = [];
+  bool _showChart = false;
 
   List<Transaction> get _recentTransactions {
     return _transactions.where((tr) {
@@ -88,13 +89,25 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    bool isLandscape =
+        MediaQuery.of(context).orientation == Orientation.landscape;
+
     final appBar = AppBar(
       title: Text('Despesas Pessoais'),
       actions: <Widget>[
+        if (isLandscape)
+          IconButton(
+            icon: Icon(_showChart ? Icons.list : Icons.show_chart),
+            onPressed: () {
+              setState(() {
+                _showChart = !_showChart;
+              });
+            },
+          ),
         IconButton(
           icon: Icon(Icons.playlist_add),
           onPressed: () => _openTransactionFormModal(context),
-        )
+        ),
       ],
     );
     final availabelHeight = MediaQuery.of(context).size.height -
@@ -107,12 +120,28 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-                height: availabelHeight * 0.25,
-                child: Chart(_recentTransactions)),
-            Container(
-                height: availabelHeight * 0.75,
-                child: TransactionList(_transactions, _removeTransaction)),
+            // if (isLandscape)
+            //   Row(
+            //     mainAxisAlignment: MainAxisAlignment.center,
+            //     children: <Widget>[
+            //       Text('Exibir Gráfico'),
+            //       Switch(
+            //           value: _showChart,
+            //           onChanged: (value) {
+            //             setState(() {
+            //               _showChart = value;
+            //             });
+            //           }),
+            //     ],
+            //   ),
+            if (_showChart || !isLandscape)
+              Container(
+                  height: availabelHeight * (isLandscape ? 0.75 : 0.25),
+                  child: Chart(_recentTransactions)),
+            if (!_showChart || !isLandscape)
+              Container(
+                  height: availabelHeight * 0.75,
+                  child: TransactionList(_transactions, _removeTransaction)),
           ],
         ),
       ),
